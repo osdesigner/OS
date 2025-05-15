6c3e38ff1a96255f4afe16b31cf53ef406cb7c2a
osdesigner <osdesigner5647@gmail.com>
2025-05-15 16:17:00 -0700

    Add files via upload

---
+++ support/scrept.js
@@ -0,0 +1,18 @@
+function toggleNav() {
+    const nav = document.getElementById("nav");
+    nav.classList.toggle("active");
+}
+
+
+  
+
+
+  document.addEventListener("DOMContentLoaded", function () {
+    const columns = document.querySelectorAll(".footer-column h4");
+    columns.forEach(column => {
+        column.addEventListener("click", function () {
+            let parent = this.parentElement;
+            parent.classList.toggle("open");
+        });
+    });
+});
