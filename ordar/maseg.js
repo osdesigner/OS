67cec150d47b13872a6a165655187cc926205f47
osdesigner <osdesigner5647@gmail.com>
2025-05-18 08:42:30 -0700

    تن

---
+++ ordar/maseg.js
@@ -0,0 +1,9 @@
+ function showTerms() {
+    document.getElementById("termsBox").classList.add("show");
+  }
+
+  function hideTerms() {
+    document.getElementById("termsBox").classList.remove("show");
+  }
+
+ 
